7d15d241b2cca349e3be833248616799a772a820
jef4490 <jeffrey.hechler@gmail.com>
2024-02-26 16:28:20 -0500

    add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
 import { useState } from 'react'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
 import museImg from './assets/muse.png'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -33,24 +42,37 @@ function App() {
       </div>
       <h1>Modal Muse</h1>
       <h2>Modal Interchange Calculator</h2>
-      <div>
-      <label>Key Center: </label>
-      <select name="key-center" id="key-center" value={keyCenter} onChange={(event) => setKeyCenter(() => event.target.value)}>
-        {selectableKeyCenters.map((pitch, i) => <option value={pitch.displayName} key={i}>{pitch.displayName}</option>)}
-      </select>
-      <br/>
-      <label>Scale Group: </label>
-      <select name="scale-group" id="scale-group" value={scaleGroup} onChange={(event) => setScaleGroup(() => event.target.value)}>
-        <option value="major" default>Major</option>
-        <option value="harmonicMinor">Harmonic Minor</option>
-        <option value="melodicMinor">Melodic Minor</option>
-      </select>
-      <br/>
-      <label>Jazz Mode: </label>
-      <label class="switch">
-        <input type="checkbox" checked={showSeventhChords} onChange={(event) => setShowSeventhChords(() => event.target.checked)}/>
-        <span class="slider round"></span>
-      </label>
+      <div className="selection-section">
+        <Box sx={{ minWidth: 120, maxWidth: 150 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Key Center</InputLabel>
+            <Select
+              value={keyCenter}
+              label="Key Center"
+              onChange={(event) => setKeyCenter(() => event.target.value)}
+            >
+              {selectableKeyCenters.map((pitch, i) => <MenuItem value={pitch.displayName} key={i}>{pitch.displayName}</MenuItem>)}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box sx={{ minWidth: 120, maxWidth: 200 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Scale Group</InputLabel>
+            <Select
+              value={keyCenter}
+              label="Scale Group"
+              value={scaleGroup}
+              onChange={(event) => setScaleGroup(() => event.target.value)}
+            >
+              <MenuItem value="major">Major</MenuItem>
+              <MenuItem value="harmonicMinor">Harmonic Minor</MenuItem>
+              <MenuItem value="melodicMinor">Melodic Minor</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <FormGroup>
+          <FormControlLabel control={<Switch checked={showSeventhChords} onChange={(event) => setShowSeventhChords(() => event.target.checked)}/>} label="Jazz Mode" />
+        </FormGroup>
       </div>
       <div className="card">
         <p>
